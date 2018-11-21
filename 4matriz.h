f23ca9fcd9b63d3fbac24a92e84929089a65d6d1
MartinezCJ <claudio.martinez@fatec.sp.gov.br>
2018-11-21 17:30:01 -0200

    Matrizes
    soma e subtração ok
    multiplicação em andamento

--- 4matriz.h
+++ 4matriz.h
@@ -1,50 +1,295 @@
 //MATRIZ//
 
 //FUNÇÕES MATRIZ//
-int define_tamanho(int linA, int linB, int colA, int colB){
+
+//FUNÇÃO SOMA//
+
+int matriz_soma(){
+	int linA=0, linB=0, colA=0, colB=0, valido=0, i=0, j=0;
 	char string[10];
-	while(linA==0)
+	
+	while(valido==0)
 	{
+		linA=0, linB=0, colA=0, colB=0;
+		while(linA==0)
+		{
+			system("cls");
+			printf("\nDigite o número de linhas da matriz A:\n");
+			so_numero(string);
+			linA=atoi(string);
+			linA=linA-1;
+		}
+		while(colA==0)
+		{
+			system("cls");
+			printf("\nDigite o número de colunas da matriz A:\n");
+			so_numero(string);
+			colA=atoi(string);
+			colA=colA-1;
+		}
+		while(linB==0)
+		{
+			system("cls");
+			printf("\nDigite o número de linhas da matriz B:\n");
+			so_numero(string);
+			linB=atoi(string);
+			linB=linB-1;
+		}
+		while(colB==0)
+		{
+			system("cls");
+			printf("\nDigite o número de colunas da matriz B:\n");
+			so_numero(string);
+			colB=atoi(string);
+			colB=colB-1;
+		}
+		
 		system("cls");
-		printf("\nDigite o número de linhas da matriz A:\n");
-		so_numero(string);
-		linA=atoi(string);
-		linA=linA-1;
-	}
-	while(colA==0)
-	{
+		if(linA==linB&&colA==colB){
+			valido=1;
+		}else
+		{
+			system("cls");
+			printf("\nMatrizes de ordem diferentes, tente novamente\n\n\n\n");
+			system("pause");
+		}
+		}
+	int mA[linA][colA];
+	int mB[linB][colB];
+	int mR[linA][colB];
+	system("cls");
+		for(i==0;i<=linA;i++){
+			j=0;
+			for(j==0;j<=colA;j++){
+				system("cls");
+				printf("\nDigite o valor %d,%d da matriz A:\n",(i+1),(j+1));
+				com_negativos(string);
+				mA[i][j]=atoi(string);
+			}
+		}
+		i=0;
+		j=0;
+		for(i==0;i<=linB;i++){
+			j=0;
+			for(j==0;j<=colB;j++){
+				system("cls");
+				printf("\nDigite o valor %d,%d da matriz B:\n",(i+1),(j+1));
+				com_negativos(string);
+				mB[i][j]=atoi(string);
+			}
+		}
+		i=0;
+		j=0;
 		system("cls");
-		printf("\nDigite o número de colunas da matriz A:\n");
-		so_numero(string);
-		colA=atoi(string);
-		colA=colA-1;
-	}
-	while(linB==0)
-	{
-		system("cls");
-		printf("\nDigite o número de linhas da matriz B:\n");
-		so_numero(string);
-		linB=atoi(string);
-		linB=linB-1;
-	}
-	while(colB==0)
-	{
-		system("cls");
-		printf("\nDigite o número de colunas da matriz B:\n");
-		so_numero(string);
-		colB=atoi(string);
-		colB=colB-1;
-	}
-	return(linA, linB, colA, colB);
+		printf("\nO resultado da soma é:\n");
+		for(i==0;i<=linA;i++){
+			j=0;
+			printf("\n");
+			for(j==0;j<=colB;j++){
+				mR[i][j]=mA[i][j]+mB[i][j];
+				printf("%d\t",mR[i][j]);
+			}
+		}
+		printf("\n\n\n\n");
+		system("pause");
+		
 }
+
+//FUNÇÃO SUBTRAI//
+
+int matriz_subtrai(){
+	int linA=0, linB=0, colA=0, colB=0, valido=0, i=0, j=0;
+	char string[10];
+	
+	while(valido==0)
+	{
+		linA=0, linB=0, colA=0, colB=0;
+		while(linA==0)
+		{
+			system("cls");
+			printf("\nDigite o número de linhas da matriz A:\n");
+			so_numero(string);
+			linA=atoi(string);
+			linA=linA-1;
+		}
+		while(colA==0)
+		{
+			system("cls");
+			printf("\nDigite o número de colunas da matriz A:\n");
+			so_numero(string);
+			colA=atoi(string);
+			colA=colA-1;
+		}
+		while(linB==0)
+		{
+			system("cls");
+			printf("\nDigite o número de linhas da matriz B:\n");
+			so_numero(string);
+			linB=atoi(string);
+			linB=linB-1;
+		}
+		while(colB==0)
+		{
+			system("cls");
+			printf("\nDigite o número de colunas da matriz B:\n");
+			so_numero(string);
+			colB=atoi(string);
+			colB=colB-1;
+		}
+		
+		system("cls");
+		if(linA==linB&&colA==colB){
+			valido=1;
+		}else
+		{
+			system("cls");
+			printf("\nMatrizes de ordem diferentes, tente novamente\n\n\n\n");
+			system("pause");
+		}
+		}
+	int mA[linA][colA];
+	int mB[linB][colB];
+	int mR[linA][colB];
+	system("cls");
+		for(i==0;i<=linA;i++){
+			j=0;
+			for(j==0;j<=colA;j++){
+				system("cls");
+				printf("\nDigite o valor %d,%d da matriz A:\n",(i+1),(j+1));
+				com_negativos(string);
+				mA[i][j]=atoi(string);
+			}
+		}
+		i=0;
+		j=0;
+		for(i==0;i<=linB;i++){
+			j=0;
+			for(j==0;j<=colB;j++){
+				system("cls");
+				printf("\nDigite o valor %d,%d da matriz B:\n",(i+1),(j+1));
+				com_negativos(string);
+				mB[i][j]=atoi(string);
+			}
+		}
+		i=0;
+		j=0;
+		system("cls");
+		printf("\nO resultado da subtração é:\n");
+		for(i==0;i<=linA;i++){
+			j=0;
+			printf("\n");
+			for(j==0;j<=colB;j++){
+				mR[i][j]=mA[i][j]-mB[i][j];
+				printf("%d\t",mR[i][j]);
+			}
+		}
+		printf("\n\n\n\n");
+		system("pause");
+		
+}
+
+//FUNÇÃO MULTIPLICA//
+
+int matriz_multiplica(){
+	int linA=0, linB=0, colA=0, colB=0, valido=0, i=0, j=0;
+	char string[10];
+	
+	while(valido==0)
+	{
+		linA=0, linB=0, colA=0, colB=0;
+		while(linA==0)
+		{
+			system("cls");
+			printf("\nDigite o número de linhas da matriz A:\n");
+			so_numero(string);
+			linA=atoi(string);
+			linA=linA-1;
+		}
+		while(colA==0)
+		{
+			system("cls");
+			printf("\nDigite o número de colunas da matriz A:\n");
+			so_numero(string);
+			colA=atoi(string);
+			colA=colA-1;
+		}
+		while(linB==0)
+		{
+			system("cls");
+			printf("\nDigite o número de linhas da matriz B:\n");
+			so_numero(string);
+			linB=atoi(string);
+			linB=linB-1;
+		}
+		while(colB==0)
+		{
+			system("cls");
+			printf("\nDigite o número de colunas da matriz B:\n");
+			so_numero(string);
+			colB=atoi(string);
+			colB=colB-1;
+		}
+		
+		system("cls");
+		if(colA==linB){
+			valido=1;
+		}else
+		{
+			system("cls");
+			printf("\nMatrizes de ordem diferentes, tente novamente\n\n\n\n");
+			system("pause");
+		}
+		}
+	int mA[linA][colA];
+	int mB[linB][colB];
+	int mR[linA][colB];
+	system("cls");
+		for(i==0;i<=linA;i++){
+			j=0;
+			for(j==0;j<=colA;j++){
+				system("cls");
+				printf("\nDigite o valor %d,%d da matriz A:\n",(i+1),(j+1));
+				com_negativos(string);
+				mA[i][j]=atoi(string);
+			}
+		}
+		i=0;
+		j=0;
+		for(i==0;i<=linB;i++){
+			j=0;
+			for(j==0;j<=colB;j++){
+				system("cls");
+				printf("\nDigite o valor %d,%d da matriz B:\n",(i+1),(j+1));
+				com_negativos(string);
+				mB[i][j]=atoi(string);
+			}
+		}
+		i=0;
+		j=0;
+		system("cls");
+		printf("\nO resultado da multiplicação é:\n");
+		for(i==0;i<=linA;i++){
+			j=0;
+			printf("\n");
+			for(j==0;j<=colB;j++){
+				mR[i][j]=mA[i][j]+mB[i][j];
+				printf("%d\t",mR[i][j]);
+			}
+		}
+		printf("\n\n\n\n");
+		system("pause");
+		
+}
+	
+		
 
 
 //MENU//
 
 int menu_matriz(){
-	char string[2], digitado[50];
-	int opcao=0, linA=0, linB=0, colA=0, colB=0, valido=0, i=0, j=0, k=0;
-	int **mA, **mB;
+	char string[2];
+	int opcao=0;
+	
 	
 	system("cls");
 	printf("\nMATRIZES\n");
@@ -58,50 +303,24 @@ int menu_matriz(){
 	
 	switch(opcao)
 	{
-	case 1:{
-		while(valido==0)
-		{
-			system("cls");
-			define_tamanho(linA, linB, colA, colB);
-			if(linA==linB&&colA==colB){
-				valido=1;
-			}else
-			{
-				system("cls");
-				printf("\nMatrizes de ordem diferentes, tente novamente\n");
-				break;
-			}
-		}
-		mA = malloc (linA * sizeof (int *));
-   	   	   for (i = 0; i < linA; ++i)
-      	  	  mA[i] = malloc (colA * sizeof (int));
-	    mB = malloc (linB * sizeof (int *));
-   	   	   for (i = 0; i < linB; ++i)
-      	  	  mB[i] = malloc (colB * sizeof (int));
-	}
+	case 1:
 		system("cls");
-		for(i==0;i<=linA;i++){
-			for(j==0;j<=colA;j++){
-				system("cls");
-				printf("\nDigite o valor %d,%d da matriz A:\n",(i+1),(j+1));
-				com_negativos(digitado);
-				k=atoi(digitado);
-				mA[i][j]=k;
-			}
-		}
-		for(i==0;i<=linB;i++){
-			for(j==0;j<=colB;j++){
-				system("cls");
-				printf("\nDigite o valor %d,%d da matriz B:\n",(i+1),(j+1));
-				com_negativos(digitado);
-				mB[i][j]=atoi(digitado);
-			}
-		}
-		printf("\n");
-		system("pause");
+		matriz_soma();
 		break;
 	
 	case 2:
+		system("cls");
+		matriz_subtrai();
+		break;
+	
+	case 3:
+		system("cls");
+		//matriz_multiplica();
+		break;
+	
+	case 4:
+		system("cls");
+		//matriz_det();
 		break;
 	
 	case 0:
